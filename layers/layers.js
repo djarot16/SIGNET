5cf9ca97bad89c9008a991617a5ef85681b200e4
djarot16 <mdarmawan170@gmail.com>
2025-06-09 15:41:32 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,55 @@
+var wms_layers = [];
+
+
+        var lyr_ESRISatelliteArcGISWorld_Imagery_0 = new ol.layer.Tile({
+            'title': 'ESRI Satellite (ArcGIS/World_Imagery)',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: ' ',
+                url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+            })
+        });
+var format_ADMINISTRASIKABUPATEN_AR_50K_1 = new ol.format.GeoJSON();
+var features_ADMINISTRASIKABUPATEN_AR_50K_1 = format_ADMINISTRASIKABUPATEN_AR_50K_1.readFeatures(json_ADMINISTRASIKABUPATEN_AR_50K_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ADMINISTRASIKABUPATEN_AR_50K_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ADMINISTRASIKABUPATEN_AR_50K_1.addFeatures(features_ADMINISTRASIKABUPATEN_AR_50K_1);
+var lyr_ADMINISTRASIKABUPATEN_AR_50K_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ADMINISTRASIKABUPATEN_AR_50K_1, 
+                style: style_ADMINISTRASIKABUPATEN_AR_50K_1,
+                popuplayertitle: 'ADMINISTRASIKABUPATEN_AR_50K',
+                interactive: true,
+                title: '<img src="styles/legend/ADMINISTRASIKABUPATEN_AR_50K_1.png" /> ADMINISTRASIKABUPATEN_AR_50K'
+            });
+var format_RumahSakit_2 = new ol.format.GeoJSON();
+var features_RumahSakit_2 = format_RumahSakit_2.readFeatures(json_RumahSakit_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_RumahSakit_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_RumahSakit_2.addFeatures(features_RumahSakit_2);
+var lyr_RumahSakit_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_RumahSakit_2, 
+                style: style_RumahSakit_2,
+                popuplayertitle: 'Rumah Sakit',
+                interactive: true,
+                title: '<img src="styles/legend/RumahSakit_2.png" /> Rumah Sakit'
+            });
+
+lyr_ESRISatelliteArcGISWorld_Imagery_0.setVisible(true);lyr_ADMINISTRASIKABUPATEN_AR_50K_1.setVisible(true);lyr_RumahSakit_2.setVisible(true);
+var layersList = [lyr_ESRISatelliteArcGISWorld_Imagery_0,lyr_ADMINISTRASIKABUPATEN_AR_50K_1,lyr_RumahSakit_2];
+lyr_ADMINISTRASIKABUPATEN_AR_50K_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_RumahSakit_2.set('fieldAliases', {'id': 'id', 'Name': 'Name', 'descriptio': 'descriptio', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMo': 'altitudeMo', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', 'Foto': 'Foto', });
+lyr_ADMINISTRASIKABUPATEN_AR_50K_1.set('fieldImages', {'KDPPUM': '', 'NAMOBJ': '', 'REMARK': '', 'KDPBPS': '', 'FCODE': '', 'LUASWH': '', 'UUPP': '', 'SRS_ID': '', 'LCODE': '', 'METADATA': '', 'KDEBPS': '', 'KDEPUM': '', 'KDCBPS': '', 'KDCPUM': '', 'KDBBPS': '', 'KDBPUM': '', 'WADMKD': '', 'WIADKD': '', 'WADMKC': '', 'WIADKC': '', 'WADMKK': '', 'WIADKK': '', 'WADMPR': '', 'WIADPR': '', 'TIPADM': '', 'SHAPE_Leng': '', 'SHAPE_Area': '', });
+lyr_RumahSakit_2.set('fieldImages', {'id': 'TextEdit', 'Name': 'TextEdit', 'descriptio': 'TextEdit', 'timestamp': 'TextEdit', 'begin': 'TextEdit', 'end': 'TextEdit', 'altitudeMo': 'TextEdit', 'tessellate': 'TextEdit', 'extrude': 'TextEdit', 'visibility': 'TextEdit', 'drawOrder': 'TextEdit', 'icon': 'TextEdit', 'Foto': 'TextEdit', });
+lyr_ADMINISTRASIKABUPATEN_AR_50K_1.set('fieldLabels', {'KDPPUM': 'hidden field', 'NAMOBJ': 'hidden field', 'REMARK': 'hidden field', 'KDPBPS': 'hidden field', 'FCODE': 'hidden field', 'LUASWH': 'hidden field', 'UUPP': 'hidden field', 'SRS_ID': 'hidden field', 'LCODE': 'hidden field', 'METADATA': 'hidden field', 'KDEBPS': 'hidden field', 'KDEPUM': 'hidden field', 'KDCBPS': 'hidden field', 'KDCPUM': 'hidden field', 'KDBBPS': 'hidden field', 'KDBPUM': 'hidden field', 'WADMKD': 'hidden field', 'WIADKD': 'hidden field', 'WADMKC': 'hidden field', 'WIADKC': 'hidden field', 'WADMKK': 'hidden field', 'WIADKK': 'hidden field', 'WADMPR': 'hidden field', 'WIADPR': 'hidden field', 'TIPADM': 'hidden field', 'SHAPE_Leng': 'hidden field', 'SHAPE_Area': 'hidden field', });
+lyr_RumahSakit_2.set('fieldLabels', {'id': 'hidden field', 'Name': 'inline label - visible with data', 'descriptio': 'hidden field', 'timestamp': 'hidden field', 'begin': 'hidden field', 'end': 'hidden field', 'altitudeMo': 'hidden field', 'tessellate': 'hidden field', 'extrude': 'hidden field', 'visibility': 'hidden field', 'drawOrder': 'hidden field', 'icon': 'hidden field', 'Foto': 'inline label - visible with data', });
+lyr_RumahSakit_2.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
